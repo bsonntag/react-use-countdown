54570ee1d5ffeb81920f1bacf4d1c3e03c77b8e9
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-17 15:13:45 +0100

    Update tests with act

--- src/index.test.js
+++ src/index.test.js
@@ -1,31 +1,39 @@
-import { render, wait } from 'react-testing-library';
+import { act, render, wait } from 'react-testing-library';
 import React from 'react';
 import useCountdown from '.';
 
 const Test = ({ date }) => {
   const timeLeft = useCountdown(() => date, { now: () => date - 1000 });
 
-  return (
-    <p>
-      {timeLeft}
-    </p>
-  );
+  return <p>{timeLeft}</p>;
 };
 
-test('should render the initial time left', () => {
-  const { container } = render(<Test date={Date.now() + 1000} />);
+describe('useCountdown', () => {
+  const setInterval = global.setInterval;
 
-  expect(container.firstChild.textContent).toBe('1000');
-});
+  beforeEach(() => {
+    global.setInterval = (callback, time) => {
+      return setInterval(() => act(callback), time);
+    };
+  });
 
-test('should eventually render 0', async () => {
-  const { container } = render(
-    <Test date={Date.now() + 1000} />
-  );
+  afterEach(() => {
+    global.setInterval = setInterval;
+  });
 
-  expect(container.firstChild.textContent).toBe('1000');
+  it('should render the initial time left', () => {
+    const { container } = render(<Test date={Date.now() + 1000} />);
 
-  await wait(() => {
-    expect(container.firstChild.textContent).toBe('0');
+    expect(container.firstChild.textContent).toBe('1000');
+  });
+
+  it('should eventually render 0', async () => {
+    const { container } = render(<Test date={Date.now() + 1000} />);
+
+    expect(container.firstChild.textContent).toBe('1000');
+
+    await wait(() => {
+      expect(container.firstChild.textContent).toBe('0');
+    });
   });
 });
