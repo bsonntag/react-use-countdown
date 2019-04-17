1f9c0437e7198672314c9c01f4c99c0d46ee931e
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-17 15:13:23 +0100

    Update babel configuration

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    test: {
+      presets: [
+        ['@babel/env', { targets: { node: true } }],
+        '@babel/react'
+      ]
+    }
+  },
+  presets: ['@babel/env', '@babel/react']
+};
